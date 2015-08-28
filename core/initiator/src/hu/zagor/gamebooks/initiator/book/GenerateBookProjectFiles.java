b0147e12a7a173db82e16b86bed8df44fe27b7d9
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2015-08-28 23:22:44 +0200

    Added book code generation verification to avoid duplications.

--- core/initiator/src/hu/zagor/gamebooks/initiator/book/GenerateBookProjectFiles.java
+++ core/initiator/src/hu/zagor/gamebooks/initiator/book/GenerateBookProjectFiles.java
@@ -2,6 +2,7 @@ package hu.zagor.gamebooks.initiator.book;
 
 import hu.zagor.gamebooks.initiator.AbstractGenerator;
 
+import java.io.File;
 import java.util.List;
 
 public class GenerateBookProjectFiles extends AbstractGenerator {
@@ -10,11 +11,24 @@ public class GenerateBookProjectFiles extends AbstractGenerator {
     private final GenerateBookMediaProjectFiles medGenerator = new GenerateBookMediaProjectFiles();
 
     public void generateBookProjectFiles(final BookBaseData baseData, final List<BookLangData> books) {
-        for (final BookLangData data : books) {
-            System.out.println("Start generating book " + data.getTitle());
-            if (data.isGeneratable()) {
-                data.init(baseData);
-                generateFiles(baseData, data);
+        boolean verificationFailed = false;
+        System.out.println("Verifying abbreviation availability for book(s).");
+
+        final File rootPath = new File("d:/System/eclipsegit/books/" + baseData.getCollectorCode() + "/presentation-" + baseData.getCollectorName());
+        final File codeDir = new File(rootPath, "src/main/java/hu/zagor/gamebooks/" + baseData.getRuleset() + "/" + baseData.getSeriesCode() + "/"
+            + baseData.getTitleCode() + "/mvc/books/");
+        if (codeDir.exists()) {
+            verificationFailed = true;
+            System.out.println("Verification failed. No code will be generated.");
+        }
+
+        if (!verificationFailed) {
+            for (final BookLangData data : books) {
+                System.out.println("Start generating book " + data.getTitle());
+                if (data.isGeneratable()) {
+                    data.init(baseData);
+                    generateFiles(baseData, data);
+                }
             }
         }
     }
