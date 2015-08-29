65e9cb74b1587344530f1722062a84b90c752d47
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2015-08-29 18:12:11 +0200

    Started implementing KJKZF2p1.

---
+++ books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/content/command/fight/roundresolver/SingleWm8FightRoundResolver.java
@@ -0,0 +1,46 @@
+package hu.zagor.gamebooks.content.command.fight.roundresolver;
+
+import hu.zagor.gamebooks.books.random.RandomNumberGenerator;
+import hu.zagor.gamebooks.content.command.fight.FightCommand;
+import hu.zagor.gamebooks.content.command.fight.domain.FightCommandMessageList;
+import hu.zagor.gamebooks.content.command.fight.roundresolver.domain.FightDataDto;
+import hu.zagor.gamebooks.renderer.DiceResultRenderer;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.stereotype.Component;
+
+/**
+ * Ff7-specific single fight round resolver.
+ * @author Tamas_Szekeres
+ */
+@Component("singlewm8FightRoundResolver")
+public class SingleWm8FightRoundResolver extends SingleFightRoundResolver {
+
+    private static final int TORNAKK_STONE_SKIL = 3;
+
+    @Autowired
+    @Qualifier("d6")
+    private RandomNumberGenerator generator;
+    @Autowired
+    private DiceResultRenderer renderer;
+
+    @Override
+    protected void damageEnemy(final FightCommand command, final FightDataDto dto) {
+        if ("2".equals(dto.getEnemy().getId())) {
+            final int[] randomNumber = generator.getRandomNumber(1);
+            final FightCommandMessageList messages = command.getMessages();
+            String result;
+            if (randomNumber[0] > TORNAKK_STONE_SKIL) {
+                result = "damage";
+            } else {
+                super.damageEnemy(command, dto);
+                result = "defense";
+            }
+            messages.addKey("page.wm8.label.fight.tornakk." + result, renderer.render(generator.getDefaultDiceSide(), randomNumber));
+        } else {
+            super.damageEnemy(command, dto);
+        }
+    }
+
+}
---
+++ books/ff/presentation-fightingfantasy/src/main/webapp/META-INF/resources/WEB-INF/tiles/ruleset/ff/wm8/charpage.jsp
@@ -0,0 +1,5 @@
+<%@page pageEncoding="utf-8" contentType="text/html; charset=utf-8"%>
+<%@taglib uri="http://java.sun.com/jsp/jstl/core" prefix="c"%>
+<%@taglib uri="http://tiles.apache.org/tags-tiles" prefix="tiles"%>
+
+<tiles:insertTemplate template="../charpage/charpage-basic.jsp" />
