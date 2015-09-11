9397adf10a6aacadefe952a959c3ee849a129a73
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2015-09-11 22:10:41 +0200

    Continued implementation of KJK2.

---
+++ books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/content/command/fight/roundresolver/SingleFf1FightRoundResolver.java
@@ -0,0 +1,43 @@
+package hu.zagor.gamebooks.content.command.fight.roundresolver;
+
+import hu.zagor.gamebooks.character.handler.item.FfCharacterItemHandler;
+import hu.zagor.gamebooks.content.command.fight.domain.FightCommandMessageList;
+import hu.zagor.gamebooks.content.command.fight.roundresolver.domain.FightDataDto;
+import hu.zagor.gamebooks.ff.character.FfCharacter;
+
+import org.springframework.stereotype.Component;
+
+/**
+ * Ff1-specific single fight round resolver.
+ * @author Tamas_Szekeres
+ */
+@Component("singleff1FightRoundResolver")
+public class SingleFf1FightRoundResolver extends SingleFightRoundResolver {
+
+    private static final int SHIELD_BLOCKS = 6;
+
+    @Override
+    protected void damageSelf(final FightDataDto dto) {
+        final FfCharacter character = dto.getCharacter();
+        character.setDamageProtection(0);
+        final FfCharacterItemHandler itemHandler = dto.getCharacterHandler().getItemHandler();
+        if (itemHandler.hasEquippedItem(character, "3005")) {
+            if (shieldBlocksDamage(dto)) {
+                character.setDamageProtection(1);
+            }
+        }
+        super.damageSelf(dto);
+    }
+
+    private boolean shieldBlocksDamage(final FightDataDto dto) {
+        boolean successfulBlock = false;
+
+        final int[] result = getGenerator().getRandomNumber(1);
+        successfulBlock = result[0] == SHIELD_BLOCKS;
+        final FightCommandMessageList messages = dto.getMessages();
+        messages.addKey("page.ff.label.random.after", getDiceResultRenderer().render(getGenerator().getDefaultDiceSide(), result), result[0]);
+        messages.addKey("page.ff1.label.shield." + successfulBlock);
+
+        return successfulBlock;
+    }
+}
--- books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/content/command/fight/roundresolver/SingleWm8FightRoundResolver.java
+++ books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/content/command/fight/roundresolver/SingleWm8FightRoundResolver.java
@@ -1,13 +1,9 @@
 package hu.zagor.gamebooks.content.command.fight.roundresolver;
 
-import hu.zagor.gamebooks.books.random.RandomNumberGenerator;
 import hu.zagor.gamebooks.content.command.fight.FightCommand;
 import hu.zagor.gamebooks.content.command.fight.domain.FightCommandMessageList;
 import hu.zagor.gamebooks.content.command.fight.roundresolver.domain.FightDataDto;
-import hu.zagor.gamebooks.renderer.DiceResultRenderer;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.stereotype.Component;
 
 /**
@@ -19,16 +15,10 @@ public class SingleWm8FightRoundResolver extends SingleFightRoundResolver {
 
     private static final int TORNAKK_STONE_SKIL = 3;
 
-    @Autowired
-    @Qualifier("d6")
-    private RandomNumberGenerator generator;
-    @Autowired
-    private DiceResultRenderer renderer;
-
     @Override
     protected void damageEnemy(final FightCommand command, final FightDataDto dto) {
         if ("2".equals(dto.getEnemy().getId())) {
-            final int[] randomNumber = generator.getRandomNumber(1);
+            final int[] randomNumber = getGenerator().getRandomNumber(1);
             final FightCommandMessageList messages = command.getMessages();
             String result;
             if (randomNumber[0] > TORNAKK_STONE_SKIL) {
@@ -37,7 +27,7 @@ public class SingleWm8FightRoundResolver extends SingleFightRoundResolver {
                 super.damageEnemy(command, dto);
                 result = "damage";
             }
-            messages.addKey("page.wm8.label.fight.tornakk." + result, renderer.render(generator.getDefaultDiceSide(), randomNumber));
+            messages.addKey("page.wm8.label.fight.tornakk." + result, getDiceResultRenderer().render(getGenerator().getDefaultDiceSide(), randomNumber));
         } else {
             super.damageEnemy(command, dto);
         }
