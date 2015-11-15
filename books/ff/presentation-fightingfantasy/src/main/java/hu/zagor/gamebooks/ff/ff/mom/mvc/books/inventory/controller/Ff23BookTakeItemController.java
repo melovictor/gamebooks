756d9a3abdfc38e73979313305592593708df972
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2015-11-15 21:13:54 +0100

    [KJK15] continued implementation

--- books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/ff/mom/mvc/books/inventory/controller/Ff23BookTakeItemController.java
+++ books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/ff/mom/mvc/books/inventory/controller/Ff23BookTakeItemController.java
@@ -1,8 +1,16 @@
 package hu.zagor.gamebooks.ff.ff.mom.mvc.books.inventory.controller;
 
 import hu.zagor.gamebooks.PageAddresses;
+import hu.zagor.gamebooks.character.Character;
+import hu.zagor.gamebooks.character.handler.item.FfCharacterItemHandler;
+import hu.zagor.gamebooks.character.item.FfItem;
+import hu.zagor.gamebooks.character.item.Item;
 import hu.zagor.gamebooks.ff.mvc.book.inventory.controller.FfBookTakeItemController;
 import hu.zagor.gamebooks.support.bookids.english.FightingFantasy;
+
+import java.util.List;
+
+import javax.servlet.http.HttpServletRequest;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -10,9 +18,41 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Controller for handling the item taking request to the given book.
  * @author Tamas_Szekeres
- *
  */
 @Controller
 @RequestMapping(value = PageAddresses.BOOK_PAGE + "/" + FightingFantasy.MASKS_OF_MAYHEM)
 public class Ff23BookTakeItemController extends FfBookTakeItemController {
+
+    private static final String FLASK = "3014";
+    private static final int PROVISION_STAMINA_BONUS = 4;
+    private static final int AMOUNT = 10;
+    private static final String NOT_EATEN_FLAG = "4002";
+    private static final String PROVISION = "2000";
+
+    @Override
+    protected String doHandleConsumeItem(final HttpServletRequest request, final String itemId) {
+        if (PROVISION.equals(itemId)) {
+            final Character character = getWrapper(request).getCharacter();
+            final FfCharacterItemHandler itemHandler = getInfo().getCharacterHandler().getItemHandler();
+            itemHandler.removeItem(character, NOT_EATEN_FLAG, AMOUNT);
+        }
+        return super.doHandleConsumeItem(request, itemId);
+    }
+
+    @Override
+    protected int doHandleItemTake(final HttpServletRequest request, final String itemId, final int amount) {
+        if (FLASK.equals(itemId)) {
+            resetProvisions(request);
+        }
+        return super.doHandleItemTake(request, itemId, amount);
+    }
+
+    private void resetProvisions(final HttpServletRequest request) {
+        final Character character = getWrapper(request).getCharacter();
+        final FfCharacterItemHandler itemHandler = getInfo().getCharacterHandler().getItemHandler();
+        final List<Item> items = itemHandler.getItems(character, PROVISION);
+        for (final Item item : items) {
+            ((FfItem) item).setStamina(PROVISION_STAMINA_BONUS);
+        }
+    }
 }
--- books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/ff/mom/mvc/books/section/controller/Ff23BookSectionController.java
+++ books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/ff/mom/mvc/books/section/controller/Ff23BookSectionController.java
@@ -1,8 +1,8 @@
 package hu.zagor.gamebooks.ff.ff.mom.mvc.books.section.controller;
 
 import hu.zagor.gamebooks.PageAddresses;
-import hu.zagor.gamebooks.mvc.book.section.service.SectionHandlingService;
 import hu.zagor.gamebooks.ff.mvc.book.section.controller.FfBookSectionController;
+import hu.zagor.gamebooks.mvc.book.section.service.SectionHandlingService;
 import hu.zagor.gamebooks.support.bookids.english.FightingFantasy;
 
 import org.springframework.beans.factory.annotation.Autowired;
@@ -13,11 +13,11 @@ import org.springframework.web.bind.annotation.RequestMapping;
 /**
  * Controller for handling the section changes in the given book.
  * @author Tamas_Szekeres
- *
  */
 @Controller
 @RequestMapping(value = PageAddresses.BOOK_PAGE + "/" + FightingFantasy.MASKS_OF_MAYHEM)
 public class Ff23BookSectionController extends FfBookSectionController {
+
     /**
      * Constructor expecting the {@link SectionHandlingService} bean.
      * @param sectionHandlingService the {@link SectionHandlingService} bean
@@ -26,4 +26,5 @@ public class Ff23BookSectionController extends FfBookSectionController {
     public Ff23BookSectionController(@Qualifier("ffSectionHandlingService") final SectionHandlingService sectionHandlingService) {
         super(sectionHandlingService);
     }
+
 }
