fb98b3e89202a102163c68250c8018fe8cc6276a
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2015-12-04 23:52:13 +0100

    [CORE] split up AuthorizationFilter
    Authorization is no longer necessary as it is being managed by the
    spring security.
    MDC setup has been moved into its own filter.
    Request logging has been moved into its own filter.

---
+++ core/gamebooks/src/main/java/hu/zagor/gamebooks/filters/MdcInitializerFilter.java
@@ -0,0 +1,38 @@
+package hu.zagor.gamebooks.filters;
+
+import hu.zagor.gamebooks.mdc.DefaultMdcHandler;
+import hu.zagor.gamebooks.mdc.MdcHandler;
+import java.io.IOException;
+import java.util.regex.Matcher;
+import java.util.regex.Pattern;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Filter for redirecting users not logged in to the login page.
+ * @author Tamas_Szekeres
+ */
+public class MdcInitializerFilter extends AbstractHttpFilter {
+
+    private static final Pattern BOOK_ID_PATTERN = Pattern.compile("\\/gamebooks\\/book\\/([0-9]+)\\/(?!resources)");
+
+    private final MdcHandler mdcHandler = new DefaultMdcHandler();
+
+    @Override
+    protected void doFilterHttp(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain) throws IOException, ServletException {
+        mdcHandler.provideUserId(request.getSession(), fetchBookId(request.getRequestURI()));
+        chain.doFilter(request, response);
+    }
+
+    private String fetchBookId(final String requestUri) {
+        String bookId = null;
+        final Matcher matcher = BOOK_ID_PATTERN.matcher(requestUri);
+        if (matcher.find()) {
+            bookId = matcher.group(1);
+        }
+        return bookId;
+    }
+
+}
---
+++ core/gamebooks/src/main/java/hu/zagor/gamebooks/filters/RequestLoggingFilter.java
@@ -0,0 +1,58 @@
+package hu.zagor.gamebooks.filters;
+
+import java.io.IOException;
+import java.util.Enumeration;
+import javax.servlet.FilterChain;
+import javax.servlet.ServletException;
+import javax.servlet.http.Cookie;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+/**
+ * Filter for redirecting users not logged in to the login page.
+ * @author Tamas_Szekeres
+ */
+public class RequestLoggingFilter extends AbstractHttpFilter {
+
+    private final Logger logger = LoggerFactory.getLogger(RequestLoggingFilter.class);
+
+    @Override
+    protected void doFilterHttp(final HttpServletRequest request, final HttpServletResponse response, final FilterChain chain) throws IOException, ServletException {
+        try {
+            if (response.getHeader("resource") == null) {
+                logRequestInfo(request);
+            }
+            chain.doFilter(request, response);
+        } catch (final Throwable ex) {
+            logger.error("Exception bubbled up:", ex);
+            throw ex;
+        }
+    }
+
+    private void logRequestInfo(final HttpServletRequest request) {
+        final Logger logger = getLogger();
+        logger.info("Requested url: '{}'", request.getRequestURL().toString());
+
+        final Enumeration<String> attributeNames = request.getAttributeNames();
+        while (attributeNames.hasMoreElements()) {
+            final String name = attributeNames.nextElement();
+            final Object value = request.getAttribute(name);
+            logger.info("Attribute '{}': '{}'.", name, value);
+        }
+
+        final Enumeration<String> parameterNames = request.getParameterNames();
+        while (parameterNames.hasMoreElements()) {
+            final String name = parameterNames.nextElement();
+            final Object value = "password".equals(name) ? "******************" : request.getParameter(name);
+            logger.info("Parameter '{}': '{}'.", name, value);
+        }
+
+        if (request.getCookies() != null) {
+            for (final Cookie cookie : request.getCookies()) {
+                logger.info("Cookie '{}': '{}'.", cookie.getName(), cookie.getValue());
+            }
+        }
+    }
+}
