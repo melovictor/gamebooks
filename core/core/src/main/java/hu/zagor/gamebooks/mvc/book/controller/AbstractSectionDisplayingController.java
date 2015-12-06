db87a909b425ae34d74402386f8d4a3c9f9bddea
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2015-12-06 22:32:56 +0100

    [CORE] title for the character generator page should always be displayed

--- core/core/src/main/java/hu/zagor/gamebooks/mvc/book/controller/AbstractSectionDisplayingController.java
+++ core/core/src/main/java/hu/zagor/gamebooks/mvc/book/controller/AbstractSectionDisplayingController.java
@@ -4,14 +4,16 @@ import hu.zagor.gamebooks.books.contentstorage.domain.BookParagraphConstants;
 import hu.zagor.gamebooks.content.Paragraph;
 import hu.zagor.gamebooks.player.PlayerSettings;
 import hu.zagor.gamebooks.player.PlayerUser;
-
+import java.util.Set;
 import org.springframework.ui.Model;
+import com.google.common.collect.Sets;
 
 /**
  * Abstract controller for controllers that are supposed to fire up sections to the users.
  * @author Tamas_Szekeres
  */
 public abstract class AbstractSectionDisplayingController extends AbstractRequestWrappingController {
+    private static final Set<String> ALWAYS_SHOW_TOP = Sets.newHashSet(BookParagraphConstants.BACKGROUND.getValue(), BookParagraphConstants.GENERATE.getValue());
 
     /**
      * Sets up some common section display options for the section pages.
@@ -21,7 +23,7 @@ public abstract class AbstractSectionDisplayingController extends AbstractReques
      */
     protected void setUpSectionDisplayOptions(final Paragraph paragraph, final Model model, final PlayerUser player) {
         final PlayerSettings settings = player.getSettings();
-        if (!BookParagraphConstants.BACKGROUND.getValue().equals(paragraph.getId())) {
+        if (!ALWAYS_SHOW_TOP.contains(paragraph.getId())) {
             model.addAttribute("hideTopSection", !settings.isTopSectionDisplayable());
         }
         model.addAttribute("hideChoiceSection", !settings.isChoiceSectionDisplayable());
