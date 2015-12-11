d6dee1d5ec67d345f5ff5c41275962e50e04b473
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2015-12-11 21:10:34 +0100

    [CORE] differentiate between login and other access denied handling

--- core/gamebooks/src/main/java/hu/zagor/gamebooks/security/GamebooksAccessDeniedHandler.java
+++ core/gamebooks/src/main/java/hu/zagor/gamebooks/security/GamebooksAccessDeniedHandler.java
@@ -1,5 +1,7 @@
 package hu.zagor.gamebooks.security;
 
+import hu.zagor.gamebooks.ControllerAddresses;
+import hu.zagor.gamebooks.PageAddresses;
 import java.io.IOException;
 import javax.servlet.ServletException;
 import javax.servlet.http.HttpServletRequest;
@@ -12,14 +14,23 @@ import org.springframework.security.web.access.AccessDeniedHandlerImpl;
  * Custom extension of the {@link AccessDeniedHandlerImpl} for handling the missing or incorrect CSRF token differently.
  * @author Tamas_Szekeres
  */
-public class CsrfAccessDeniedHandler extends AccessDeniedHandlerImpl {
+public class GamebooksAccessDeniedHandler extends AccessDeniedHandlerImpl {
 
     @Override
     public void handle(final HttpServletRequest request, final HttpServletResponse response, final AccessDeniedException accessDeniedException)
         throws IOException, ServletException {
-        final String messageKey = "page.login.invalid.token";
-        request.getSession().setAttribute(WebAttributes.AUTHENTICATION_EXCEPTION, new Exception(messageKey));
-        response.sendRedirect("login");
+
+        if (isAuthenticated(request)) {
+            response.sendRedirect(PageAddresses.BOOK_LIST);
+        } else {
+            final String messageKey = "page.login.invalid.token";
+            request.getSession().setAttribute(WebAttributes.AUTHENTICATION_EXCEPTION, new Exception(messageKey));
+            response.sendRedirect(PageAddresses.LOGIN);
+        }
+    }
+
+    private boolean isAuthenticated(final HttpServletRequest request) {
+        return request.getSession().getAttribute(ControllerAddresses.USER_STORE_KEY) != null;
     }
 
 }
