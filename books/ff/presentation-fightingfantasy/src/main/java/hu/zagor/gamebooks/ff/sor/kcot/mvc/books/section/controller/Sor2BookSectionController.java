7e8f255cdb5a5c0f4279e18b719402846fa751b8
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-01-13 22:57:40 +0100

    [KJV2] continued implementation

--- books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/sor/kcot/mvc/books/section/controller/Sor2BookSectionController.java
+++ books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/sor/kcot/mvc/books/section/controller/Sor2BookSectionController.java
@@ -1,6 +1,9 @@
 package hu.zagor.gamebooks.ff.sor.kcot.mvc.books.section.controller;
 
 import hu.zagor.gamebooks.PageAddresses;
+import hu.zagor.gamebooks.character.Character;
+import hu.zagor.gamebooks.character.handler.item.FfCharacterItemHandler;
+import hu.zagor.gamebooks.controller.session.HttpSessionWrapper;
 import hu.zagor.gamebooks.ff.mvc.book.section.controller.SorBookSectionController;
 import hu.zagor.gamebooks.mvc.book.section.service.SectionHandlingService;
 import hu.zagor.gamebooks.support.bookids.english.Sorcery;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = PageAddresses.BOOK_PAGE + "/" + Sorcery.KHARE_CITYPORT_OF_TRAPS)
 public class Sor2BookSectionController extends SorBookSectionController {
+
     /**
      * Constructor expecting the {@link SectionHandlingService} bean.
      * @param sectionHandlingService the {@link SectionHandlingService} bean
@@ -23,5 +27,15 @@ public class Sor2BookSectionController extends SorBookSectionController {
     @Autowired
     public Sor2BookSectionController(@Qualifier("ffSectionHandlingService") final SectionHandlingService sectionHandlingService) {
         super(sectionHandlingService);
+    }
+
+    @Override
+    protected void handleAfterFight(final HttpSessionWrapper wrapper, final String enemyId) {
+        final Character character = wrapper.getCharacter();
+        if (character.getCommandView() == null) {
+            final FfCharacterItemHandler itemHandler = getInfo().getCharacterHandler().getItemHandler();
+            itemHandler.removeItem(character, "4015", 1);
+            itemHandler.removeItem(character, "4016", 1);
+        }
     }
 }
--- books/ff/presentation-fightingfantasy/src/test/java/hu/zagor/gamebooks/ff/sor/kcot/mvc/books/section/controller/Sor2BookSectionControllerTest.java
+++ books/ff/presentation-fightingfantasy/src/test/java/hu/zagor/gamebooks/ff/sor/kcot/mvc/books/section/controller/Sor2BookSectionControllerTest.java
@@ -1,9 +1,19 @@
 package hu.zagor.gamebooks.ff.sor.kcot.mvc.books.section.controller;
 
+import static org.easymock.EasyMock.expect;
+import hu.zagor.gamebooks.character.handler.FfCharacterHandler;
+import hu.zagor.gamebooks.character.handler.item.FfCharacterItemHandler;
+import hu.zagor.gamebooks.content.command.CommandView;
+import hu.zagor.gamebooks.controller.session.HttpSessionWrapper;
+import hu.zagor.gamebooks.domain.FfBookInformations;
+import hu.zagor.gamebooks.ff.character.SorCharacter;
 import hu.zagor.gamebooks.mvc.book.section.service.SectionHandlingService;
-
-import org.easymock.EasyMock;
+import hu.zagor.gamebooks.support.mock.annotation.Inject;
+import hu.zagor.gamebooks.support.mock.annotation.Instance;
+import hu.zagor.gamebooks.support.mock.annotation.MockControl;
+import hu.zagor.gamebooks.support.mock.annotation.UnderTest;
 import org.easymock.IMocksControl;
+import org.easymock.Mock;
 import org.testng.annotations.AfterMethod;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.BeforeMethod;
@@ -15,14 +25,24 @@ import org.testng.annotations.Test;
  */
 @Test
 public class Sor2BookSectionControllerTest {
-
-    private IMocksControl mockControl;
-    private SectionHandlingService sectionHandler;
+    private Sor2BookSectionController underTest;
+    @MockControl private IMocksControl mockControl;
+    @Mock private SectionHandlingService sectionHandler;
+    @Mock private HttpSessionWrapper wrapper;
+    @Mock private SorCharacter character;
+    @Mock private CommandView commandView;
+    @Inject private FfBookInformations info;
+    @Instance private FfCharacterHandler characterHandler;
+    @Mock private FfCharacterItemHandler itemHandler;
 
     @BeforeClass
     public void setUpClass() {
-        mockControl = EasyMock.createStrictControl();
-        sectionHandler = mockControl.createMock(SectionHandlingService.class);
+        characterHandler.setItemHandler(itemHandler);
+    }
+
+    @UnderTest
+    public Sor2BookSectionController underTest() {
+        return new Sor2BookSectionController(sectionHandler);
     }
 
     @BeforeMethod
@@ -39,11 +59,26 @@ public class Sor2BookSectionControllerTest {
         // THEN throws exception
     }
 
-    public void testConstructorWhenSectionHandlerProvidedShouldCreateObject() {
+    public void testHandleAfterFightWhenFightIsOngoingShouldDoNothing() {
         // GIVEN
+        expect(wrapper.getCharacter()).andReturn(character);
+        expect(character.getCommandView()).andReturn(commandView);
         mockControl.replay();
         // WHEN
-        new Sor2BookSectionController(sectionHandler).getClass();
+        underTest.handleAfterFight(wrapper, "10");
+        // THEN
+    }
+
+    public void testHandleAfterFightWhenFightIsOverShouldRemoveSingleBattleSkillBonuses() {
+        // GIVEN
+        expect(wrapper.getCharacter()).andReturn(character);
+        expect(character.getCommandView()).andReturn(null);
+        expect(info.getCharacterHandler()).andReturn(characterHandler);
+        itemHandler.removeItem(character, "4015", 1);
+        itemHandler.removeItem(character, "4016", 1);
+        mockControl.replay();
+        // WHEN
+        underTest.handleAfterFight(wrapper, "10");
         // THEN
     }
 
