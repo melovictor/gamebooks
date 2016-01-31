66309a6ab60df090d9938ad4a4efec56a1bbc22f
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-01-31 21:18:59 +0100

    [RS-FF] small change about the list type for the forced weapons

--- rulesets/rs-ff/src/main/java/hu/zagor/gamebooks/books/contentransforming/section/stub/FightTransformer.java
+++ rulesets/rs-ff/src/main/java/hu/zagor/gamebooks/books/contentransforming/section/stub/FightTransformer.java
@@ -8,12 +8,10 @@ import hu.zagor.gamebooks.content.choice.ChoicePositionCounter;
 import hu.zagor.gamebooks.content.command.Command;
 import hu.zagor.gamebooks.content.command.fight.FightCommand;
 import hu.zagor.gamebooks.content.command.fight.domain.WeaponReplacementData;
-
 import java.util.ArrayList;
 import java.util.Arrays;
 import java.util.List;
 import java.util.Map;
-
 import org.springframework.util.StringUtils;
 import org.w3c.dom.Node;
 import org.w3c.dom.NodeList;
@@ -40,12 +38,14 @@ public class FightTransformer extends AbstractStubTransformer {
         fightCommand.setAttackStrengthRolledDices(extractIntegerAttribute(node, "attackStrengthRolledDices", 2));
         fightCommand.setAttackStrengthUsedDices(extractIntegerAttribute(node, "attackStrengthUsedDices", 2));
         fightCommand.setRecoverDamage(extractBooleanAttribute(node, "recoverDamage", false));
-        fightCommand.setUsableWeaponTypes(toWeaponTypes(extractAttribute(node, "usableWeaponTypes",
-            "shooting".equals(fightCommand.getBattleType()) ? "shooting" : "weapon1,weapon2").split(",")));
+        fightCommand.setUsableWeaponTypes(
+            toWeaponTypes(extractAttribute(node, "usableWeaponTypes", "shooting".equals(fightCommand.getBattleType()) ? "shooting" : "weapon1,weapon2").split(",")));
         final String forceWeapon = extractAttribute(node, "forceWeapon");
         if (!StringUtils.isEmpty(forceWeapon)) {
             final WeaponReplacementData replacementData = new WeaponReplacementData();
-            replacementData.setForceWeapons(Arrays.asList(forceWeapon.split(",")));
+            final ArrayList<String> forcedWeapons = new ArrayList<>();
+            forcedWeapons.addAll(Arrays.asList(forceWeapon.split(",")));
+            replacementData.setForceWeapons(forcedWeapons);
             fightCommand.setReplacementData(replacementData);
         }
         fightCommand.setPreFightAvailable(extractBooleanAttribute(node, "preFight", true));
