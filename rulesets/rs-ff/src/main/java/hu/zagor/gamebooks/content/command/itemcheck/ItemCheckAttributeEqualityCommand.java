ed452eb426fa98d8a79a179e0d1e85d672d85227
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-02-21 15:30:42 +0100

    [RS-FF] fine-tuning to accept comma separated list as well

--- rulesets/rs-ff/src/main/java/hu/zagor/gamebooks/content/command/itemcheck/ItemCheckAttributeEqualityCommand.java
+++ rulesets/rs-ff/src/main/java/hu/zagor/gamebooks/content/command/itemcheck/ItemCheckAttributeEqualityCommand.java
@@ -4,6 +4,8 @@ import hu.zagor.gamebooks.character.domain.ResolvationData;
 import hu.zagor.gamebooks.character.handler.FfCharacterHandler;
 import hu.zagor.gamebooks.content.ParagraphData;
 import hu.zagor.gamebooks.ff.character.FfCharacter;
+import java.util.Arrays;
+import java.util.List;
 
 /**
  * Class for resolving an attribute-type {@link ItemCheckCommand} where we expect equality, instead of greater-than-or-equal-to.
@@ -16,8 +18,11 @@ public class ItemCheckAttributeEqualityCommand extends ItemCheckAttributeCommand
         final FfCharacterHandler characterHandler = (FfCharacterHandler) resolvationData.getCharacterHandler();
         ParagraphData toResolve;
 
-        final String attribute = parent.getCheckType().toString();
-        if (getAttributeValue((FfCharacter) resolvationData.getCharacter(), characterHandler, attribute) == Integer.parseInt(parent.getId())) {
+        final String attributeName = parent.getCheckType().toString();
+        final String acceptedValues = parent.getId();
+        final List<String> acceptedList = Arrays.asList(acceptedValues.split(","));
+        final int attributeValue = getAttributeValue((FfCharacter) resolvationData.getCharacter(), characterHandler, attributeName);
+        if (acceptedList.contains(String.valueOf(attributeValue))) {
             toResolve = parent.getHave();
         } else {
             toResolve = parent.getDontHave();
