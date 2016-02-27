acc83e4b4e2371f6df5da72d6d67da7e1dca7c65
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-02-28 00:12:49 +0100

    [CORE] added missing file for content based image type detection

--- core/support/src/main/java/hu/zagor/gamebooks/support/imagetype/ContentBasedImageTypeDetector.java
+++ core/support/src/main/java/hu/zagor/gamebooks/support/imagetype/ContentBasedImageTypeDetector.java
@@ -12,13 +12,15 @@ public class ContentBasedImageTypeDetector implements ImageTypeDetector {
     @Override
     public String probeContentType(final InputStream resourceInputStream, final OutputStream outputStream) throws IOException {
         String type = null;
-        final String content = readContent(resourceInputStream, outputStream);
-        if (content.contains("JFIF") || content.contains("Exif")) {
-            type = "image/jpeg";
-        } else if (content.contains("GIF89a")) {
-            type = "image/gif";
-        } else if (content.contains("PNG")) {
-            type = "image/png";
+        if (resourceInputStream != null && outputStream != null) {
+            final String content = readContent(resourceInputStream, outputStream);
+            if (content.contains("JFIF") || content.contains("Exif")) {
+                type = "image/jpeg";
+            } else if (content.contains("GIF89a")) {
+                type = "image/gif";
+            } else if (content.contains("PNG")) {
+                type = "image/png";
+            }
         }
         return type;
     }
