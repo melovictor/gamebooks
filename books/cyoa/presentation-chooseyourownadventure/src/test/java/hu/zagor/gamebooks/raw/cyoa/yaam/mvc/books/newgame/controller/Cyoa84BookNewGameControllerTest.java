d17bf9b3dd147eef89ef9319f80acb4d8f7eedd3
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-03-23 19:20:25 +0100

    [CYOA84] added new book

---
+++ books/cyoa/presentation-chooseyourownadventure/src/test/java/hu/zagor/gamebooks/raw/cyoa/yaam/mvc/books/newgame/controller/Cyoa84BookNewGameControllerTest.java
@@ -0,0 +1,18 @@
+package hu.zagor.gamebooks.raw.cyoa.yaam.mvc.books.newgame.controller;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for class {@link Cyoa84BookNewGameController}.
+ * @author Tamas_Szekeres
+ */
+@Test
+public class Cyoa84BookNewGameControllerTest {
+
+    public void testConstructor() {
+        // GIVEN
+        // WHEN
+        new Cyoa84BookNewGameController().getClass();
+        // THEN
+    }
+}
---
+++ books/cyoa/presentation-chooseyourownadventure/src/test/java/hu/zagor/gamebooks/raw/cyoa/yaam/mvc/books/section/controller/Cyoa84BookWelcomeControllerTest.java
@@ -0,0 +1,18 @@
+package hu.zagor.gamebooks.raw.cyoa.yaam.mvc.books.section.controller;
+
+import org.testng.annotations.Test;
+
+/**
+ * Unit test for class {@link Cyoa84BookWelcomeController}.
+ * @author Tamas_Szekeres
+ */
+@Test
+public class Cyoa84BookWelcomeControllerTest {
+
+    public void testConstructor() {
+        // GIVEN
+        // WHEN
+        new Cyoa84BookWelcomeController().getClass();
+        // THEN
+    }
+}
