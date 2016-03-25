0cccfa2bc7a605aae9795430e4df2688c8451460
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-03-25 20:43:29 +0100

    [INIT] that's the wrong way of generating the char page

--- core/initiator/src/hu/zagor/gamebooks/initiator/book/GenerateBookMediaProjectFiles.java
+++ core/initiator/src/hu/zagor/gamebooks/initiator/book/GenerateBookMediaProjectFiles.java
@@ -1,9 +1,7 @@
 package hu.zagor.gamebooks.initiator.book;
 
 import hu.zagor.gamebooks.initiator.AbstractGenerator;
-import hu.zagor.gamebooks.initiator.Console;
 import java.io.File;
-import java.io.IOException;
 
 public class GenerateBookMediaProjectFiles extends AbstractGenerator {
 
@@ -13,29 +11,8 @@ public class GenerateBookMediaProjectFiles extends AbstractGenerator {
     }
 
     private void generateMediaProject(final File medRootPath, final BookBaseData baseData, final BookLangData data) {
-        final Console console = Console.getConsole();
-
-        try {
-
-            createDir(medRootPath, "src/main/resources/" + baseData.getSeriesCode() + baseData.getPosition());
-            createDir(medRootPath, "src/main/resources/" + baseData.getSeriesCode() + baseData.getPosition() + data.getLang());
-
-            if (baseData.hasInventory()) {
-                final File presRootPath = new File("c:/springsource/eclipsegit/books/" + baseData.getCollectorCode() + "/presentation-" + baseData.getCollectorName());
-                createDir(presRootPath, "src/main/resources/WEB-INF/tiles/ruleset/" + baseData.getSeriesCode() + "/" + baseData.getSeriesCode() + baseData.getPosition());
-                createFile(presRootPath, "src/main/resources/WEB-INF/tiles/ruleset/" + baseData.getSeriesCode() + "/" + baseData.getSeriesCode() + baseData.getPosition(),
-                    "charpage.jsp", getCharpageContent());
-            }
-        } catch (final IOException exception) {
-            console.print("Failed to create all necessary files.");
-            exception.printStackTrace(System.out);
-        }
-    }
-
-    private String getCharpageContent() {
-        return "<%@page pageEncoding=\"utf-8\" contentType=\"text/html; charset=utf-8\"%>\r\n"
-            + "<%@taglib uri=\"http://tiles.apache.org/tags-tiles\" prefix=\"tiles\"%>\r\n" + "\r\n"
-            + "<tiles:insertTemplate template=\"../charpage/charpage-basic.jsp\" />\r\n";
+        createDir(medRootPath, "src/main/resources/" + baseData.getSeriesCode() + baseData.getPosition());
+        createDir(medRootPath, "src/main/resources/" + baseData.getSeriesCode() + baseData.getPosition() + data.getLang());
     }
 
 }
