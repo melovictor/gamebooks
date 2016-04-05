7d8c1acc5abddaadd4a576b595d087a70ff4d054
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-04-05 21:08:43 +0200

    [KJV4] continued implementation

--- books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/sor/tcok/mvc/books/inventory/controller/Sor4BookTakeItemController.java
+++ books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/sor/tcok/mvc/books/inventory/controller/Sor4BookTakeItemController.java
@@ -1,8 +1,14 @@
 package hu.zagor.gamebooks.ff.sor.tcok.mvc.books.inventory.controller;
 
 import hu.zagor.gamebooks.PageAddresses;
+import hu.zagor.gamebooks.content.command.market.MarketCommand;
+import hu.zagor.gamebooks.content.command.market.domain.MarketElement;
+import hu.zagor.gamebooks.controller.session.HttpSessionWrapper;
 import hu.zagor.gamebooks.ff.mvc.book.inventory.controller.SorBookTakeItemController;
 import hu.zagor.gamebooks.support.bookids.english.Sorcery;
+import java.util.Map;
+import javax.annotation.Resource;
+import javax.servlet.http.HttpServletRequest;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -13,4 +19,18 @@ import org.springframework.web.bind.annotation.RequestMapping;
 @Controller
 @RequestMapping(value = PageAddresses.BOOK_PAGE + "/" + Sorcery.THE_CROWN_OF_KINGS)
 public class Sor4BookTakeItemController extends SorBookTakeItemController {
+    @Resource(name = "sor4MagicItemsPriceChange") private Map<Integer, Integer> priceChange;
+
+    @Override
+    protected Map<String, Object> doHandleMarketBuy(final HttpServletRequest request, final String itemId) {
+        final Map<String, Object> doHandleMarketBuy = super.doHandleMarketBuy(request, itemId);
+
+        final HttpSessionWrapper wrapper = getWrapper(request);
+        final MarketCommand market = (MarketCommand) wrapper.getParagraph().getItemsToProcess().get(0).getCommand();
+        for (final MarketElement element : market.getItemsForSale()) {
+            element.setPrice(priceChange.get(element.getPrice()));
+        }
+
+        return doHandleMarketBuy;
+    }
 }
