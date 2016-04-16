cce3ea7b4f0ae17bc4b8cf2ce9ddb9a85208fcf7
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-04-16 12:02:07 +0200

    [GYG] fixed title

--- core/support/src/main/java/hu/zagor/gamebooks/support/bookids/english/GiveYourselfGoosebumps.java
+++ core/support/src/main/java/hu/zagor/gamebooks/support/bookids/english/GiveYourselfGoosebumps.java
@@ -4,7 +4,7 @@ public class GiveYourselfGoosebumps {
 
     private static final long BASE = Series.GIVE_YOURSELF_GOOSEBUMPS;
 
-    public static final String TITLE = "Give Yourslef Goosebumps";
+    public static final String TITLE = "Give Yourself Goosebumps";
 
     public static final long TICK_TOCK_YOURE_DEAD = BASE + 2;
     public static final long TRAPPED_IN_BAT_WING_HALL = BASE + 3;
