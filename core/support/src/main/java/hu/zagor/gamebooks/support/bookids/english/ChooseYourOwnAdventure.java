add013a37bc1035e0f456501d1df6c9ecd04f77b
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-04-18 09:03:27 +0200

    [CYOA] added new titles

--- core/support/src/main/java/hu/zagor/gamebooks/support/bookids/english/ChooseYourOwnAdventure.java
+++ core/support/src/main/java/hu/zagor/gamebooks/support/bookids/english/ChooseYourOwnAdventure.java
@@ -55,15 +55,61 @@ public final class ChooseYourOwnAdventure {
 
     public static final long MYSTERY_OF_THE_SECRET_ROOM = BASE + 63;
 
+    public static final long THE_MARDI_GRAS_MYSTERY = BASE + 65;
+
     public static final long SECRET_OF_THE_SUN_GOD = BASE + 68;
+
+    public static final long INVADERS_OF_THE_PLANET_EARTH = BASE + 70;
+
+    public static final long LONGHORN_TERRITORY = BASE + 74;
+    public static final long PLANET_OF_THE_DRAGONS = BASE + 75;
+
+    public static final long THE_PERFECT_PLANET = BASE + 80;
 
     public static final long YOU_ARE_A_MONSTER = BASE + 84;
 
+    public static final long EXILED_TO_EARTH = BASE + 87;
     public static final long MASTER_OF_KUNG_FU = BASE + 88;
+
+    public static final long THROUGH_THE_BLACK_HOLE = BASE + 97;
+
+    public static final long MASTER_OF_TAE_KWON_DO = BASE + 102;
+
+    public static final long HIJACKED = BASE + 106;
+    public static final long FIGHT_FOR_FREEDOM = BASE + 107;
+
+    public static final long SMOKE_JUMPER = BASE + 111;
+
+    public static final long DAREDEVIL_PARK = BASE + 114;
+
+    public static final long GHOST_TRAIN = BASE + 120;
 
     public static final long EARTHQUAKE = BASE + 129;
 
+    public static final long THE_FORGOTTEN_PLANET = BASE + 133;
+
+    public static final long SCENE_OF_THE_CRIME = BASE + 137;
+
+    public static final long THE_REAILTY_MACHINE = BASE + 142;
+
+    public static final long CYBERSPACE_WARRIORS = BASE + 154;
+
+    public static final long UN_ADVENTURE = BASE + 157;
+    public static final long SKY_JAM = BASE + 158;
+
+    public static final long TYPHOON = BASE + 162;
+
     public static final long MASTER_OF_AIKIDO = BASE + 166;
+
+    public static final long TERROR_ON_THE_TITANIC = BASE + 169;
+
+    public static final long THE_POWER_DOME = BASE + 174;
+
+    public static final long STAMPEDE = BASE + 180;
+    public static final long FIRE_ON_ICE = BASE + 181;
+
+    public static final long CYBERHACKER = BASE + 183;
+    public static final long MAYDAY = BASE + 184;
 
     private ChooseYourOwnAdventure() {
     }
