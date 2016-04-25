acca9b8e37826a8ba4a1e0920d98a196439b2c88
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-04-25 21:38:33 +0200

    [KJKZ4] continued implementation

--- books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/content/command/fight/roundresolver/SingleFf60FightRoundResolver.java
+++ books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/content/command/fight/roundresolver/SingleFf60FightRoundResolver.java
@@ -1,9 +1,9 @@
 package hu.zagor.gamebooks.content.command.fight.roundresolver;
 
+import hu.zagor.gamebooks.books.random.RandomNumberGenerator;
 import hu.zagor.gamebooks.character.Character;
 import hu.zagor.gamebooks.character.domain.ResolvationData;
 import hu.zagor.gamebooks.character.enemy.FfEnemy;
-import hu.zagor.gamebooks.character.handler.CharacterHandler;
 import hu.zagor.gamebooks.character.handler.item.CharacterItemHandler;
 import hu.zagor.gamebooks.character.handler.userinteraction.FfUserInteractionHandler;
 import hu.zagor.gamebooks.content.ParagraphData;
@@ -15,6 +15,7 @@ import hu.zagor.gamebooks.content.command.fight.domain.FightRoundResult;
 import hu.zagor.gamebooks.content.command.random.RandomCommand;
 import hu.zagor.gamebooks.ff.character.FfCharacter;
 import hu.zagor.gamebooks.ff.mvc.book.section.controller.domain.LastFightCommand;
+import hu.zagor.gamebooks.renderer.DiceResultRenderer;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -24,40 +25,63 @@ import org.springframework.beans.factory.annotation.Qualifier;
  * @author Tamas_Szekeres
  */
 public class SingleFf60FightRoundResolver implements FightRoundResolver {
+    private static final int HOOK_MAN_EXTRA_HIT = 4;
     private static final String OGRE_ID = "4";
     @Autowired @Qualifier("singleFightRoundResolver") private FightRoundResolver decorated;
     @Autowired @Qualifier("randomCommandResolver") private SilentCapableResolver<RandomCommand> randomCommandResolver;
     private RandomCommand ogreRandomCommand;
+    @Autowired @Qualifier("d6") private RandomNumberGenerator generator;
+    @Autowired private DiceResultRenderer renderer;
 
     @Override
     public FightRoundResult[] resolveRound(final FightCommand command, final ResolvationData resolvationData, final FightBeforeRoundResult beforeRoundResult) {
-        final CharacterHandler characterHandler = resolvationData.getCharacterHandler();
-        final Character character = resolvationData.getCharacter();
-
         final FfEnemy enemy = getEnemy(resolvationData);
 
         final FightRoundResult[] results = decorated.resolveRound(command, resolvationData, beforeRoundResult);
 
         if (enemyIsOgre(enemy)) {
-            final CharacterItemHandler itemHandler = characterHandler.getItemHandler();
-            itemHandler.removeItem(character, "4001", 2);
-            if (results[0] == FightRoundResult.LOSE) {
-                final FightCommandMessageList messages = command.getMessages();
-                try {
-                    messages.switchToPostRoundMessages();
-                    final List<ParagraphData> randomResult = randomCommandResolver.resolveSilently(ogreRandomCommand.clone(), resolvationData, messages,
-                        messages.getLocale());
-                    if (!randomResult.isEmpty()) {
-                        itemHandler.addItem(character, "4001", 2);
-                        messages.addKey("page.ff60.fight.ogre.push");
-                    }
-                } catch (final CloneNotSupportedException ex) {
-                    throw new IllegalStateException("Failed to clone random command.", ex);
-                }
-            }
+            handleOgre(command, resolvationData, results);
+        } else if (enemyIsHookMan(enemy) && results[0] == FightRoundResult.LOSE) {
+            handleHookMan(command, resolvationData);
         }
 
         return results;
+    }
+
+    private void handleHookMan(final FightCommand command, final ResolvationData resolvationData) {
+        final FightCommandMessageList messages = command.getMessages();
+        messages.switchToPostRoundMessages();
+        final int[] randomNumber = generator.getRandomNumber(1);
+        final String dice = renderer.render(generator.getDefaultDiceSide(), randomNumber);
+        messages.addKey("page.ff.label.random.after", dice, randomNumber[0]);
+        if (randomNumber[0] > HOOK_MAN_EXTRA_HIT) {
+            final FfCharacter character = (FfCharacter) resolvationData.getCharacter();
+            character.changeStamina(-1);
+        }
+    }
+
+    private boolean enemyIsHookMan(final FfEnemy enemy) {
+        return "19".equals(enemy.getId());
+    }
+
+    private void handleOgre(final FightCommand command, final ResolvationData resolvationData, final FightRoundResult[] results) {
+        final Character character = resolvationData.getCharacter();
+        final CharacterItemHandler itemHandler = resolvationData.getCharacterHandler().getItemHandler();
+        itemHandler.removeItem(character, "4001", 2);
+        if (results[0] == FightRoundResult.LOSE) {
+            final FightCommandMessageList messages = command.getMessages();
+            try {
+                messages.switchToPostRoundMessages();
+                final List<ParagraphData> randomResult = randomCommandResolver.resolveSilently(ogreRandomCommand.clone(), resolvationData, messages,
+                    messages.getLocale());
+                if (!randomResult.isEmpty()) {
+                    itemHandler.addItem(character, "4001", 2);
+                    messages.addKey("page.ff60.fight.ogre.push");
+                }
+            } catch (final CloneNotSupportedException ex) {
+                throw new IllegalStateException("Failed to clone random command.", ex);
+            }
+        }
     }
 
     private boolean enemyIsOgre(final FfEnemy enemy) {
