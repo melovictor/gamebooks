1c72f9937083b4d73d931845232d34fb713839dd
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-05-02 00:04:49 +0200

    [KJKZ4] continued implementation

--- books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/ff/b/mvc/books/inventory/controller/Ff60BookTakeItemController.java
+++ books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/ff/b/mvc/books/inventory/controller/Ff60BookTakeItemController.java
@@ -2,6 +2,7 @@ package hu.zagor.gamebooks.ff.ff.b.mvc.books.inventory.controller;
 
 import hu.zagor.gamebooks.PageAddresses;
 import hu.zagor.gamebooks.controller.session.HttpSessionWrapper;
+import hu.zagor.gamebooks.ff.ff.b.character.Ff60Character;
 import hu.zagor.gamebooks.ff.mvc.book.inventory.controller.FfBookTakeItemController;
 import hu.zagor.gamebooks.support.bookids.english.FightingFantasy;
 import org.springframework.stereotype.Controller;
@@ -19,9 +20,23 @@ public class Ff60BookTakeItemController extends FfBookTakeItemController {
     protected String doHandleConsumeItem(final HttpSessionWrapper wrapper, final String itemId) {
         if ("3013".equals(itemId)) {
             getInfo().getCharacterHandler().getItemHandler().addItem(wrapper.getCharacter(), "4000", 2);
+        } else if ("2001".equals(itemId)) {
+            getInfo().getCharacterHandler().getItemHandler().addItem(wrapper.getCharacter(), "4000", 1);
+        } else if ("2002".equals(itemId)) {
+            handleAntivenom(wrapper);
         }
 
         return super.doHandleConsumeItem(wrapper, itemId);
+    }
+
+    private void handleAntivenom(final HttpSessionWrapper wrapper) {
+        final Ff60Character character = (Ff60Character) wrapper.getCharacter();
+        final String paragraphId = wrapper.getParagraph().getId();
+        if ("235".equals(paragraphId) && character.getNineTailDamage() > 0) {
+            final int half = character.getNineTailDamage() / 2;
+            character.changeStamina(half);
+            character.setNineTailDamage(0);
+        }
     }
 
 }
