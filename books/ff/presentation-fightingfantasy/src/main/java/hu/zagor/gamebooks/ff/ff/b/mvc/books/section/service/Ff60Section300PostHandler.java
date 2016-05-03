a1468e448f5010627fb4320670d1151ef47e4617
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-05-03 23:23:00 +0200

    [KJKZ4] continued implementation

---
+++ books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/ff/b/mvc/books/section/service/Ff60Section300PostHandler.java
@@ -0,0 +1,22 @@
+package hu.zagor.gamebooks.ff.ff.b.mvc.books.section.service;
+
+import hu.zagor.gamebooks.controller.session.HttpSessionWrapper;
+import hu.zagor.gamebooks.domain.FfBookInformations;
+import hu.zagor.gamebooks.ff.character.FfCharacter;
+import hu.zagor.gamebooks.ff.mvc.book.section.service.FfCustomPrePostSectionHandler;
+import org.springframework.ui.Model;
+
+/**
+ * Post section handler for FF60, section 300.
+ * @author Tamas_Szekeres
+ */
+public class Ff60Section300PostHandler extends FfCustomPrePostSectionHandler {
+
+    @Override
+    public void handle(final Model model, final HttpSessionWrapper wrapper, final FfBookInformations info, final boolean changedSection) {
+        final FfCharacter character = (FfCharacter) wrapper.getCharacter();
+        final int stamina = info.getCharacterHandler().getAttributeHandler().resolveValue(character, "stamina");
+        character.changeStamina(-stamina / 2);
+    }
+
+}
