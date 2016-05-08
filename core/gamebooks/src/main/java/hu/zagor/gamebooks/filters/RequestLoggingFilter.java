1aabc85ea46ea41db4a13ea748babced49828aee
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-05-08 13:59:49 +0200

    [CORE] removed attribute and cookie logging
    They doesn't seem to have any added value to the log.

--- core/gamebooks/src/main/java/hu/zagor/gamebooks/filters/RequestLoggingFilter.java
+++ core/gamebooks/src/main/java/hu/zagor/gamebooks/filters/RequestLoggingFilter.java
@@ -4,7 +4,6 @@ import java.io.IOException;
 import java.util.Enumeration;
 import javax.servlet.FilterChain;
 import javax.servlet.ServletException;
-import javax.servlet.http.Cookie;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.slf4j.Logger;
@@ -35,24 +34,11 @@ public class RequestLoggingFilter extends AbstractHttpFilter {
         final Logger logger = getLogger();
         logger.info("Requested url: '{}'", request.getRequestURL().toString());
 
-        final Enumeration<String> attributeNames = request.getAttributeNames();
-        while (attributeNames.hasMoreElements()) {
-            final String name = attributeNames.nextElement();
-            final Object value = request.getAttribute(name);
-            logger.info("Attribute '{}': '{}'.", name, value);
-        }
-
         final Enumeration<String> parameterNames = request.getParameterNames();
         while (parameterNames.hasMoreElements()) {
             final String name = parameterNames.nextElement();
             final Object value = "password".equals(name) ? "******************" : request.getParameter(name);
             logger.info("Parameter '{}': '{}'.", name, value);
-        }
-
-        if (request.getCookies() != null) {
-            for (final Cookie cookie : request.getCookies()) {
-                logger.info("Cookie '{}': '{}'.", cookie.getName(), cookie.getValue());
-            }
         }
     }
 }
