be722d181deb93d6b29503856054b5ed0f3d2c2c
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-05-20 23:00:04 +0200

    [KJK10] continued implementation

--- books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/ff/tod/mvc/books/section/service/fight/DeathKnightHandler.java
+++ books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/ff/tod/mvc/books/section/service/fight/DeathKnightHandler.java
@@ -1,38 +1,33 @@
 package hu.zagor.gamebooks.ff.ff.tod.mvc.books.section.service.fight;
 
 import hu.zagor.gamebooks.character.domain.ResolvationData;
-import hu.zagor.gamebooks.character.enemy.Enemy;
 import hu.zagor.gamebooks.content.command.fight.FightCommand;
 import hu.zagor.gamebooks.content.command.fight.domain.FightRoundResult;
-import java.util.Map;
 import org.springframework.stereotype.Component;
 
 /**
- * Captain handler for ff11.
+ * Death Knight handler for FF11.
  * @author Tamas_Szekeres
  */
 @Component
-public class CaptainHandler extends Ff11BeforeAfterRoundEnemyHandler {
-    private static final int CAPTAIN_CRITICAL_HIT_LIMIT = 18;
+public class DeathKnightHandler extends Ff11BeforeAfterRoundEnemyHandler {
+    private static final int DEATH_KNIGHT_STAMINA_LIMIT = 6;
 
     @Override
     public boolean shouldExecutePostHandler(final FightCommand command, final ResolvationData resolvationData, final FightRoundResult[] results,
         final EnemyPrePostFightDataContainer data) {
-        return command.getEnemies().contains("7") && command.getAttackStrengths().containsKey("7") && command.getAttackStrengths().get("7") >= CAPTAIN_CRITICAL_HIT_LIMIT
-            && results[0] == FightRoundResult.LOSE;
+        return resolvationData.getCharacterHandler().getAttributeHandler().resolveValue(resolvationData.getCharacter(), "stamina") <= DEATH_KNIGHT_STAMINA_LIMIT;
     }
 
     @Override
     public void executePostHandler(final FightCommand command, final ResolvationData resolvationData, final FightRoundResult[] results,
         final EnemyPrePostFightDataContainer data) {
-        final Map<String, Enemy> enemies = resolvationData.getEnemies();
-        killEnemy(enemies.get("7"));
-        killEnemy(enemies.get("8"));
-        resolvationData.getCharacterHandler().getItemHandler().addItem(resolvationData.getCharacter(), "4001", 1);
+        killEnemy(resolvationData.getEnemies().get("10"));
+        resolvationData.getCharacterHandler().getItemHandler().addItem(resolvationData.getCharacter(), "4002", 1);
     }
 
     @Override
     protected String getEnemyId() {
-        return "7";
+        return "10";
     }
 }
--- books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/ff/tod/mvc/books/section/service/fight/Ff11BeforeAfterRoundEnemyHandler.java
+++ books/ff/presentation-fightingfantasy/src/main/java/hu/zagor/gamebooks/ff/ff/tod/mvc/books/section/service/fight/Ff11BeforeAfterRoundEnemyHandler.java
@@ -1,5 +1,7 @@
 package hu.zagor.gamebooks.ff.ff.tod.mvc.books.section.service.fight;
 
+import hu.zagor.gamebooks.character.enemy.Enemy;
+import hu.zagor.gamebooks.character.enemy.FfEnemy;
 import hu.zagor.gamebooks.content.command.fight.enemyroundresolver.BasicBeforeAfterRoundEnemyHandler;
 
 /**
@@ -7,4 +9,13 @@ import hu.zagor.gamebooks.content.command.fight.enemyroundresolver.BasicBeforeAf
  * @author Tamas_Szekeres
  */
 public abstract class Ff11BeforeAfterRoundEnemyHandler extends BasicBeforeAfterRoundEnemyHandler<EnemyPrePostFightDataContainer> {
+
+    /**
+     * Kills the provided enemy.
+     * @param enemyObject the enemy to kill
+     */
+    protected void killEnemy(final Enemy enemyObject) {
+        final FfEnemy enemy = (FfEnemy) enemyObject;
+        enemy.setStamina(0);
+    }
 }
