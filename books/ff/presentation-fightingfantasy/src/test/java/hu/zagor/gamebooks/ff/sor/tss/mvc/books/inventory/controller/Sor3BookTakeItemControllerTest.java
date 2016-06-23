78f9006efba87746b7b062b3ddcae6fdc8962d95
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-06-23 08:48:53 +0200

    [SOR3] added unit test

--- books/ff/presentation-fightingfantasy/src/test/java/hu/zagor/gamebooks/ff/sor/tss/mvc/books/inventory/controller/Sor3BookTakeItemControllerTest.java
+++ books/ff/presentation-fightingfantasy/src/test/java/hu/zagor/gamebooks/ff/sor/tss/mvc/books/inventory/controller/Sor3BookTakeItemControllerTest.java
@@ -4,9 +4,11 @@ import static org.easymock.EasyMock.expect;
 import hu.zagor.gamebooks.books.random.RandomNumberGenerator;
 import hu.zagor.gamebooks.character.handler.FfCharacterHandler;
 import hu.zagor.gamebooks.character.handler.item.FfCharacterItemHandler;
+import hu.zagor.gamebooks.character.item.Item;
 import hu.zagor.gamebooks.content.Paragraph;
 import hu.zagor.gamebooks.content.ProcessableItemHolder;
 import hu.zagor.gamebooks.content.command.market.MarketCommand;
+import hu.zagor.gamebooks.content.command.market.domain.MarketElement;
 import hu.zagor.gamebooks.controller.session.HttpSessionWrapper;
 import hu.zagor.gamebooks.domain.FfBookInformations;
 import hu.zagor.gamebooks.ff.character.SorCharacter;
@@ -20,6 +22,7 @@ import hu.zagor.gamebooks.support.mock.annotation.Instance;
 import hu.zagor.gamebooks.support.mock.annotation.MockControl;
 import hu.zagor.gamebooks.support.mock.annotation.UnderTest;
 import java.util.Arrays;
+import java.util.List;
 import javax.servlet.http.HttpServletRequest;
 import org.easymock.IMocksControl;
 import org.easymock.Mock;
@@ -50,12 +53,17 @@ public class Sor3BookTakeItemControllerTest {
     @Mock private Paragraph paragraph;
     private FfBookInformations info;
     @Instance private FfCharacterHandler characterHandler;
-    private FfCharacterItemHandler itemHandler;
+    @Mock private FfCharacterItemHandler itemHandler;
     @Mock private SorCharacter character;
     @Inject private MarketHandler marketHandler;
     @Inject private ItemInteractionRecorder itemInteractionRecorder;
     @Mock private MarketCommand marketCommand;
     private BuySellResponse buySellResponse;
+    @Mock private List<MarketElement> itemsForSale;
+    @Instance private MarketElement marketA;
+    @Instance private MarketElement marketB;
+    @Instance private MarketElement marketC;
+    @Mock private Item removedItem;
 
     @BeforeClass
     public void setUpClass() {
@@ -63,6 +71,13 @@ public class Sor3BookTakeItemControllerTest {
         Whitebox.setInternalState(underTest, "info", info);
         info.setCharacterHandler(characterHandler);
         characterHandler.setItemHandler(itemHandler);
+        itemsForSale = Arrays.asList(marketA, marketB, marketC);
+        marketA.setId("3004");
+        marketA.setPrice(2);
+        marketB.setId("3005");
+        marketB.setPrice(4);
+        marketC.setId("3006");
+        marketC.setPrice(9);
     }
 
     public void testDoHandleMarketBuyWhenOnRandomSectionDoUsual() {
@@ -70,17 +85,40 @@ public class Sor3BookTakeItemControllerTest {
         expectWrapper();
         expect(wrapper.getParagraph()).andReturn(paragraph);
         expect(paragraph.getId()).andReturn("75");
+        handleDefaultMarketing();
+        mockControl.replay();
+        // WHEN
+        final BuySellResponse returned = underTest.doHandleMarketBuy(request, "3005");
+        // THEN
+        Assert.assertSame(returned, buySellResponse);
+    }
+
+    public void testDoHandleBarketBuyWhenBuyingFromTheElvesAndHasMarkerShouldEnsureItIsTradedAsFreeAndRemoveMarker() {
+        // GIVEN
+        expectWrapper();
+        expect(wrapper.getParagraph()).andReturn(paragraph);
+        expect(paragraph.getId()).andReturn("315a");
+        expect(wrapper.getCharacter()).andReturn(character);
+        expect(itemHandler.hasItem(character, "4069")).andReturn(true);
+        expect(paragraph.getItemsToProcess()).andReturn(Arrays.asList(new ProcessableItemHolder(marketCommand)));
+        expect(marketCommand.getItemsForSale()).andReturn(itemsForSale);
+        expect(itemHandler.addItem(character, "gold", 4)).andReturn(4);
+        expect(itemHandler.removeItem(character, "4069", 1)).andReturn(Arrays.asList(removedItem));
+        handleDefaultMarketing();
+        mockControl.replay();
+        // WHEN
+        final BuySellResponse returned = underTest.doHandleMarketBuy(request, "3005");
+        // THEN
+        Assert.assertSame(returned, buySellResponse);
+    }
+
+    private void handleDefaultMarketing() {
         expectWrapper();
         expect(wrapper.getCharacter()).andReturn(character);
         expect(wrapper.getParagraph()).andReturn(paragraph);
         expect(paragraph.getItemsToProcess()).andReturn(Arrays.asList(new ProcessableItemHolder(marketCommand)));
         expect(marketHandler.handleMarketPurchase("3005", character, marketCommand, characterHandler)).andReturn(buySellResponse);
         itemInteractionRecorder.recordItemMarketMovement(wrapper, "Sale", "3005");
-        mockControl.replay();
-        // WHEN
-        final BuySellResponse returned = underTest.doHandleMarketBuy(request, "3005");
-        // THEN
-        Assert.assertSame(returned, buySellResponse);
     }
 
     private void expectWrapper() {
