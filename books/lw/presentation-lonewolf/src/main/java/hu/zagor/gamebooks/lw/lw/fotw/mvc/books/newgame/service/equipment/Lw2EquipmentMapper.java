b9ee1bd8bb83ee28e8d4767dead077f7157e1fd4
Tamas Szekeres <firefoxpdm@yahoo.co.uk>
2016-08-16 23:07:43 +0200

    [LW] added null check for the equipment list
    In some cases these are optionally selectable and so the user might not
    have ticked in anything.

--- books/lw/presentation-lonewolf/src/main/java/hu/zagor/gamebooks/lw/lw/fotw/mvc/books/newgame/service/equipment/Lw2EquipmentMapper.java
+++ books/lw/presentation-lonewolf/src/main/java/hu/zagor/gamebooks/lw/lw/fotw/mvc/books/newgame/service/equipment/Lw2EquipmentMapper.java
@@ -8,6 +8,7 @@ import hu.zagor.gamebooks.lw.mvc.book.newgame.domain.LwCharGenEquipment;
 import hu.zagor.gamebooks.lw.mvc.book.newgame.domain.LwCharGenInput;
 import hu.zagor.gamebooks.lw.mvc.book.newgame.service.equipment.LwEquipmentMapper;
 import hu.zagor.gamebooks.renderer.DiceResultRenderer;
+import java.util.List;
 import java.util.Map;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -28,8 +29,11 @@ public class Lw2EquipmentMapper implements LwEquipmentMapper {
         final int[] goldCrowns = generator.getRandomNumber(d10Configuration);
         final int crowns = 10 + goldCrowns[0];
 
-        for (final LwCharGenEquipment equipment : input.getEquipments()) {
-            itemHandler.addItem(character, equipment.getId(), equipment.getAmount());
+        final List<LwCharGenEquipment> equipments = input.getEquipments();
+        if (equipments != null) {
+            for (final LwCharGenEquipment equipment : equipments) {
+                itemHandler.addItem(character, equipment.getId(), equipment.getAmount());
+            }
         }
 
         result.put("lwGoldCrowns", crowns + diceRenderer.render(DICE_SIDE, goldCrowns));
